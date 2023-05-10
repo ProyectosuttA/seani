b5a2279fadbed127a4fb0950daf71e264f64a442
alejandro <alejandro@kali>
2023-05-10 00:21:49 -0600

    Cambio de debug en settings

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2%cu(9i4v@!2r03ifqxxjf!dm9b7m)=vr-5lq6fcu*pzwo3m7='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =True
+DEBUG =False
 
 ALLOWED_HOSTS = ['*']
 
